47998dbf15113ce5b7cf1deded14f518fafc9420
bguiang <bernardguiang@gmail.com>
2021-09-01 20:51:33 -0700

    first commit

---
+++ src/main/java/com/bernardguiang/allspeak/AllspeakApplication.java
@@ -0,0 +1,13 @@
+package com.bernardguiang.allspeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllspeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllspeakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bernardguiang/allspeak/AllspeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardguiang.allspeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllspeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
